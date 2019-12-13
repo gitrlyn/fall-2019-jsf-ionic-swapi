3ecf7471bc5e3fb6664c3dc0f424729d5d25ac48
gitrlyn <ellexiong@gmail.com>
2019-12-12 18:14:43 -0600

    First part of demo done

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { SharedDataService } from '../shared-data.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  // constructor dependency injection - inject service into constructor
+  constructor(private sharedDataSvc: SharedDataService) {}
+
+  
+}
